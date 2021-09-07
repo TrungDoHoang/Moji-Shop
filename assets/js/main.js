c3b69e1f0741a22f0e578cabdbb8b38daa74d9ed
trungdohoang <hoangtrung15012k@gmail.com>
2021-09-07 11:45:31 +0700

    fix shop pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,35 @@
 $(document).ready(function () {
+    $('.new-products-more').click(function () {
+        $('.more').css('display', 'flex')
+        $('.new-products-more').hide()
+    })
+    $('.header-cart').click(() => {
+        $('.header-cart-list').toggleClass('open')
+
+    })
     TweenMax.staggerFrom(
         $('.product'), // phần tử được chọn
         1, // thời gian chuyển động
         { top: 100, opacity: 0 },
         0.2 // thời gian cách nhau giữa mỗi hiệu ứng
     )
-    $('.new-products-more').click(function () {
-        $('.more').css('display', 'flex')
-        $('.new-products-more').hide()
+    
+    // Multi carousel
+    let multiCarousel = document.querySelector('.multi-carousel')
+    let items = multiCarousel.querySelectorAll('.carousel .carousel-item')
+
+    items.forEach((el) => {
+        const minPerSlide = 4
+        let next = el.nextElementSibling
+        for (var i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
     })
 });
-let multiCarousel = document.querySelector('.multi-carousel')
-let items = multiCarousel.querySelectorAll('.carousel .carousel-item')
-
-items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i = 1; i < minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
-        }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
-
-$('.header-cart').click(() => {
-    $('.header-cart-list').toggleClass('open')
-    
-})
 
